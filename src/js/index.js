15addc2965d4a9edee48b0440e4a137df5e37989
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-01-19 16:35:06 +0200

    start

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,13 @@ const refs = getRefs();
 console.log(refs.form);
 console.log(refs.todoList);
 
+refs.todoList.addEventListener('click', onToDoElement)
 
 let todos = [
     { id: 1, label: 'template text', checked: true },
-    { id: 1, label: 'template text', checked: false },
-    { id: 1, label: 'template text', checked: false },
-    { id: 1, label: 'template text', checked: false },
+    { id: 2, label: 'template text', checked: false },
+    { id: 3, label: 'template text', checked: false },
+    { id: 4, label: 'template text', checked: false },
 ];
 
 function startRenderToDo(todos) {
@@ -23,3 +24,30 @@ function startRenderToDo(todos) {
 };
 
 startRenderToDo(todos)
+
+function deleteToDo(id) {
+    console.log("delete");
+};
+
+function toggleToDoCheck(id) {
+    console.log('toggle');
+}
+
+function onToDoElement(e) {
+    if (e.target.nodeName === 'UL') {
+        console.log('ne tuda tuknul');
+        return
+    }
+    else {
+        const { id } = e.target.closest('li').dataset
+        switch (e.target.nodeName) {
+            case 'BUTTON':
+                deleteToDo(id);
+                break;
+            case 'INPUT':
+            case 'LABLE':
+                toggleToDoCheck(id);
+                break;
+        }
+    }
+}
