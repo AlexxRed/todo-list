f4cfb3bf8ab92de10eb04e0007d4ed6a106955ff
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-01-21 17:30:13 +0200

    localstorage add

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -4,5 +4,9 @@ export const getRefs = () => {
         todoList: document.querySelector('.list-group'),
         print: document.querySelector('.btn.btn-success'),
         addToDo: document.querySelector('.btn.btn-primary'),
+        // cancelDelete: deleteModal.element().querySelector('.btn-cancel'),
+        // approveDelete: deleteModal.element().querySelector('.btn-delete'),
+        // cancelDelete: document.querySelector('.btn-cancel'),
+        approveDelete: document.querySelector('.btn-delete'),
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,43 @@
 import { v4 as uuidv4 } from 'uuid';
-import { getRefs } from './getRefs'
-import {itemTemplate} from './todoTamplate'
+import { getRefs } from './getRefs';
+import { itemTemplate } from './todoTamplate';
+import toastr from 'toastr';
+import * as basicLightbox from 'basiclightbox';
+
+import 'toastr/build/toastr.min.css';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+
+const deleteModal = basicLightbox.create(`
+<div class="delete-modal">
+	<h1>Do you really want to delete this task?</h1>
+	<p id="text">lorem ipsum</p>
+  <button class="btn btn-cancel">Cancel</button>
+  <button class="btn btn-delete">Delete</button>
+</div>
+`);
+
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "3000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
 const refs = getRefs();
-
-console.log(refs.form);
-console.log(refs.todoList);
-console.log(refs.addToDo);
-
+let currentId; 
+let currentToDoName;
 
 let todos = [
     { id: '1', label: 'template text', checked: true },
@@ -15,23 +46,66 @@ let todos = [
     { id: '4', label: 'template text', checked: false },
 ];
 
+function onCancelDeleteToDo() {
+    deleteModal.close()
+}
+
+function onApproveDeleteToDo() {
+    todos.map((todo) => {
+        if (todo.id === currentId) {
+            currentToDoName = todo.label
+        }
+    })
+    todos = todos.filter((todo) => todo.id !== currentId);
+    deleteModal.close()
+    startRenderToDo()
+    toastr.warning(`Your task - "${currentToDoName}" delete`)
+}
+
+
+
+function saveToDoList() {
+    localStorage.setItem('todos', JSON.stringify(todos))
+};
+
+function loadToDoList() {
+    try {
+        todos =JSON.parse(localStorage.getItem('todos')) 
+    }
+    catch {
+        todos = [
+    { id: '1', label: 'change your task', checked: true },
+];
+    }
+}
+
 function startRenderToDo() {
     const markUpItems = todos.map((todo) => {
         return itemTemplate(todo)
     });
     refs.todoList.innerHTML = '';
-    refs.todoList.insertAdjacentHTML('beforeend', markUpItems.join(''))
+    refs.todoList.insertAdjacentHTML('beforeend', markUpItems.join(''));
+
+    saveToDoList()
 };
 
-startRenderToDo()
+
 
 function deleteToDo(id) {
-    todos = todos.filter((todo) => todo.id !== id);
+    deleteModal.show()
+    currentId = id
+
+    const cancelDelete = document.querySelector('.btn-cancel');
+    const approveDelete = document.querySelector('.btn-delete');
+    cancelDelete.addEventListener('click', onCancelDeleteToDo);
+    approveDelete.addEventListener('click', onApproveDeleteToDo)
+
     // console.log("delete");
 };
 
 function toggleToDoCheck(id) {
     todos = todos.map((todo) => todo.id === id ? { ...todo, checked: !todo.checked } : todo);
+    toastr.success('Have you already done this?')
     // console.log('toggle');
 }
 
@@ -80,6 +154,17 @@ function onSubmit(e) {
 }
 
 
-refs.todoList.addEventListener('click', onToDoElement);
-refs.print.addEventListener('click', onPrint);
-refs.form.addEventListener('submit', onSubmit)
+
+function startRenderToDoList() {
+    loadToDoList()
+
+    refs.todoList.addEventListener('click', onToDoElement);
+    refs.print.addEventListener('click', onPrint);
+    refs.form.addEventListener('submit', onSubmit);
+
+    startRenderToDo()
+}
+
+startRenderToDoList()
+toastr.info('Create your Task')
+
