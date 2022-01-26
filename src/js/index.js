7313044cfbb4b6bd60ea41175e1536ed12194d20
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-01-27 00:09:03 +0200

    basic

--- src/js/index.js
+++ src/js/index.js
@@ -18,9 +18,9 @@ let currentWarninigName;
 const deleteModal = basicLightbox.create(`
 <div class="delete-modal">
 	<h1>Do you really want to delete this task?</h1>
-	<p id="text">item</p>
-  <button class="btn btn-cancel btn-success">Cancel</button>
-  <button class="btn btn-delete btn-danger">Delete</button>
+	<p id="text" class="modal-text">item</p>
+<button class="btn btn-cancel btn-success">Cancel</button>
+<button class="btn btn-delete btn-danger">Delete</button>
 </div>
 `);
 
@@ -182,9 +182,6 @@ function startRenderToDoList() {
     startRenderToDo()
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
 
 startRenderToDoList()
 toastr.info('Create your Task')
